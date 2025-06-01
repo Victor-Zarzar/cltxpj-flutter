56bf8de29da323000f9ed3fcd20e48cabaf71010
victorzarzar <victorzarzar58@gmail.com>
2025-06-01 10:44:58 -0300

    Add arquitecture and app_page

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationService extends StatefulWidget {
+  const NotificationService({super.key});
+
+  @override
+  State<NotificationService> createState() => _NotificationServiceState();
+}
+
+class _NotificationServiceState extends State<NotificationService> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/view/app_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppPage extends StatefulWidget {
+  const AppPage({super.key});
+
+  @override
+  State<AppPage> createState() => _AppPageState();
+}
+
+class _AppPageState extends State<AppPage> with TickerProviderStateMixin {
+  late TabController tabController;
+  final GlobalKey _tabBarKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
